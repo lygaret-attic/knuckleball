f1862c452fed521f9285ba2b1b6b6f79633c724d
Jon Raphaelson <jonraphaelson@gmail.com>
2012-11-14 00:16:05 -0700

    start of a server component
    this is a buffered, line-oriented server. it'll be the backbone of the
    smtp receiver.

---
+++ knuckleball/lib/server.go
@@ -0,0 +1,98 @@
+package lib
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+/*
+	The handle to the connected client that's passed into the handler of the
+	server. It's a line oriented interface.
+*/
+type IClient interface {
+	Readln() (s *string, err error)
+	Writeln(s string) (err error)
+}
+
+/*
+	Starts a server at the target address (http://localhost:2345) that will
+	handle connections by calling the handler in a go routine. The client
+	in the handler is an IClient, and has method to read and write in a line
+	oriented fashion.
+
+	Returns an error if there was one, or nil otherwise
+*/
+func Listen(target string, handler func(client IClient)) error {
+
+	// resolve the address
+	addr, err := net.ResolveTCPAddr("tcp", target)
+	if err != nil {
+		return err
+	}
+
+	// listen on that TCP address
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	defer listener.Close()
+
+	// accept and handle connections
+	for {
+		connection, err := listener.AcceptTCP()
+		if err != nil {
+			return err
+		}
+
+		go func() {
+			log.Println("Accepted incoming connection: ", connection.RemoteAddr(), " -> ", connection.LocalAddr())
+
+			client := sClient{connection, bufio.NewReader(connection), false}
+			defer client.close()
+
+			handler(client)
+		}()
+	}
+
+	return nil
+}
+
+func (client sClient) Readln() (*string, error) {
+
+	// read the line from the buffer
+	line, err := client.reader.ReadString('\n')
+	if err != nil {
+		log.Println("Error reading from ", client.connection.RemoteAddr(), " : ", err)
+		client.close()
+	}
+
+	return &line, err
+}
+
+func (client sClient) Writeln(line string) error {
+
+	// write to the connection stream
+	_, err := fmt.Fprintln(client.connection, line)
+	if err != nil {
+		log.Println("Error writing to ", client.connection.RemoteAddr(), " : ", err)
+		client.close()
+	}
+
+	return err
+}
+
+func (client sClient) close() {
+	client.connection.Close()
+	client.closed = true
+}
+
+// private
+
+type sClient struct {
+	connection *net.TCPConn
+	reader     *bufio.Reader
+	closed     bool
+}
